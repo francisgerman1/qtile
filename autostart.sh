c0ec22ddf5f892283fcafaea7304d9f57d2f1078
Francis Espindola <francisgerman1@gmail.com>
2022-09-25 20:30:54 -0300

    configs

---
+++ autostart.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# ---
+# Use "run program" to run it only if it is not already running
+# Use "program &" to run it regardless
+# ---
+# NOTE: This script runs with every restart of AwesomeWM
+# TODO: run_once
+
+
+function run {
+    if ! pgrep $1 > /dev/null ;
+    then
+        $@&
+    fi
+}
+
+run nitrogen --restore
+run picom -b
